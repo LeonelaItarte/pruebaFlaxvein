ca0199cfe3876b8cea5ffbd87c9644a2dff776e4
leonela itarte <leonelaitarte23@gmail.com>
2023-01-06 21:03:47 -0300

    asdasd

---
+++ index.js
@@ -0,0 +1,13 @@
+const type= new Typed('.typed',{
+    strings:['<h1 class="typed">Desarrollo De Software a Medida</h1><p class="texto">¿Los servicios clásicos de administración de negocio no te alcanzan?¿Necesitas resolver una necesidad en especifico?¿Tenes una idea pero no sabes como hacerla real? entonces FLAXVEIN es para vos! llevamos tu negocio al siguiente nivel.</p>'],
+    typeSpeed:30,
+   
+    
+});
+/*const texto= new Typed('.texto',{
+   strings:['<p>¿Los servicios clásicos de administración de negocio no te alcanzan?¿Necesitas resolver una necesidad en especifico?¿Tenes una idea pero no sabes como hacerla real? entonces FLAXVEIN es para vos! llevamos tu negocio al siguiente nivel.</p>'],
+   typeSpeed:150,
+    
+});*/
+
+// fuente mona sans
